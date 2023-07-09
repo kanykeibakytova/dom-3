b25abc4eb4f19eb2ad936f9295b3faba52210161
kanykeibakytova <kanykeib826@gmail.com>
2023-07-09 15:20:43 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+//keydown =>  клавиатураны басканда иштейт
+// keyup => клавиатураны кое бергенде иштейт
+
+
+// const input =document.querySelector("input")
+// const btn =document.querySelector("button")
+//
+// input.addEventListener("keydown", (e)=>{
+//     if(e.key==="Shift"){
+//         alert("Success")
+//         console.log(input.value)
+//     }
+// })
+
+// const h1=document.querySelector("h1")
+// 1 variant
+// h1.innerText="World"
+// h1.style.display="none"
+
+// input.addEventListener("keydown", (e)=>{
+//     if(e.key==="Enter"){
+//         h1.style.display="block"
+//         h1.style.color="red"
+//     }
+// })
+
+// 2 variant
+
+// input.addEventListener("keydown", (e)=>{
+//     if(e.key==="Enter"){
+//         h1.innerText= input.value
+//     }
+// })
+
